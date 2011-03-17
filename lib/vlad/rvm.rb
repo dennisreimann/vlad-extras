4b79f2d9a9d72235da18b7e2748948eeb7c5fbd8
dbloete <mail@dennisbloete.de>
2011-03-17 11:48:15 +0100

    Added rvm tasks

---
+++ lib/vlad/rvm.rb
@@ -0,0 +1,28 @@
+require 'vlad'
+
+namespace :vlad do
+  
+  namespace :rvm do
+    
+    set :rvm_path, "/usr/local/rvm"
+    
+    desc "Initializes the RVM environment"
+    task :init do
+      run "source #{rvm_path}/scripts/rvm"
+    end
+    
+    namespace :rvmrc do
+      desc "Trusts the .rvmrc in the current release path"
+      task :trust do
+        run "rvm rvmrc trust #{current_release}"
+      end
+      
+      desc "Changes to the release path so that the .rvmrc gets loaded"
+      task :load do
+        run "cd #{release_path}"
+      end
+    end
+    
+  end
+  
+end
