a5bc151f1dc81a4f7fa63167cb1fb2d5118ea814
Danil Pismenny <danil@orionet.ru>
2011-06-06 20:08:27 +0400

    DelayedJob support

--- lib/vlad/delayed_job.rb
+++ lib/vlad/delayed_job.rb
@@ -5,11 +5,13 @@ namespace :vlad do
   namespace :delayed_job do 
     
     set :delayed_job_cmd, 'script/delayed_job'
+    set :dj_processes_count, nil 
     
     %w(start restart stop).each do |task|
       desc "#{task.capitalize} the delayed_job process"
       remote_task task.to_sym, :roles => :app do
-        run "cd #{current_path}; RAILS_ENV=#{rails_env} #{delayed_job_cmd} #{task}"
+        processes = dj_processes_count and task=~/start/ ? "-n #{dj_processes_count}" : ''
+        run "cd #{current_path}; RAILS_ENV=#{rails_env} #{delayed_job_cmd} #{task} #{processes}"
       end
     end
   end
