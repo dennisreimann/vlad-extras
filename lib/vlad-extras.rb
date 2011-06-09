d9bdabf4202daf70926c5c58d1a96ebd09ff99b8
Danil Pismenny <danil@orionet.ru>
2011-06-09 17:32:40 +0400

    Remove 'writer' from default recipies because of problem with mongoid

--- lib/vlad-extras.rb
+++ lib/vlad-extras.rb
@@ -1,4 +1,4 @@
 # Load extra recipes
-%w(symlinks monit writer delayed_job).each do |recipe|
+%w(symlinks monit copy bundle rvm deploy delayed_job).each do |recipe|
   require File.dirname(__FILE__) + "/vlad/#{recipe}.rb"
 end
