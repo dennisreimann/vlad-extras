3d5db6cacc3dbc5107376a6ac0960ade300ed7aa
Danil Pismenny <danil@orionet.ru>
2011-06-09 12:10:07 +0400

    Use Array or Hash for symlinks

--- lib/vlad/symlinks.rb
+++ lib/vlad/symlinks.rb
@@ -1,13 +1,26 @@
 require 'vlad'
 
 namespace :vlad do
-  
+
   set :symlinks, {}
-  
+
   desc "Symlinks files"
   remote_task :symlink, :roles => :web do
-    symlinks.each_pair do |source, destination|
-      run "ln -s #{shared_path}/#{source} #{current_path}/#{destination}"
+    if symlinks.is_a? Hash
+      symlinks.each_pair do |source, destination|
+        run "ln -s #{shared_path}/#{source} #{current_path}/#{destination}"
+      end
+    else
+      symlinks.each do |file|
+        run "ln -s #{shared_path}/#{file} #{current_path}/#{file}"
+      end
     end
   end
+
+  remote_task :update do
+    # Rake::Task['vlad:symlink'].invoke
+    # Rake::Task['vlad:bundle_install'].invoke
+    # Rake::Task['vlad:trust_scm_rvm'].invoke
+  end
+
 end
