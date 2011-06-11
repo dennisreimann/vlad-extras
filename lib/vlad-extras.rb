274a8f74e9f47e423d3d0a8d70ad824688b8549e
Danil Pismenny <danil@orionet.ru>
2011-06-11 08:55:51 +0400

    Methods to overwrite and remove rake tasks

--- lib/vlad-extras.rb
+++ lib/vlad-extras.rb
@@ -2,3 +2,20 @@
 %w(symlinks monit copy bundle rvm deploy delayed_job).each do |recipe|
   require File.dirname(__FILE__) + "/vlad/#{recipe}.rb"
 end
+
+# http://blog.jayfields.com/2008/02/rake-task-overwriting.html
+class Rake::Task
+  def overwrite(&block)
+    @actions.clear
+    enhance(&block)
+  end
+end
+
+# http://matthewbass.com/2007/03/07/overriding-existing-rake-tasks/
+# http://rubyizednrailified.blogspot.com/2008/07/remove-rake-tasks.html
+Rake::TaskManager.class_eval do
+  def remove_task(task_name)
+    @tasks.delete(task_name.to_s)
+  end
+end
+
