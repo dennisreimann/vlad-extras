a311a548a7f0392efe5ca42899a54eb2cc80a8a5
dbloete <mail@dennisreimann.de>
2011-10-26 22:36:39 +0200

    Refactored copy recipe

--- lib/vlad/copy.rb
+++ lib/vlad/copy.rb
@@ -1,24 +1,32 @@
 # encoding: utf-8
 require 'vlad'
+require 'vlad-extras/copy'
 
 namespace :vlad do
 
-  set :copy_files, []
+  namespace :copy do
 
-  desc "Copy selected files into remote shared directory"
-  task :copy do
-    print "Copying files to server: "
-    copy_files.each do |file|
-      print "#{file}, "
-      dir = File.dirname(file)
-      `ssh #{domain} 'mkdir -p #{shared_path}/#{dir}'` if dir
-      `scp -r #{file} #{domain}:#{shared_path}/#{file}`
+    set :copy_files, {}
+    set :copy_shared, {}
+
+    desc "Copy files to remote"
+    task :files do
+      puts "Copying files to remote:"
+      copy_files.each_pair do |local, remote|
+        puts "#{local} -> #{remote}"
+        VladExtras::Copy.local_to_remote(local, remote)
+      end
     end
-    puts "Done"
-  end
 
-  task :setup do
-    Rake::Task['vlad:copy'].invoke
+    desc "Copy files into remote shared directory"
+    task :shared do
+      puts "Copying files into remote shared directory:"
+      copy_shared.each_pair do |local, remote|
+        puts "#{local} -> shared/#{remote}"
+        VladExtras::Copy.local_to_remote(local, "#{shared_path}/#{remote}")
+      end
+    end
+
   end
 
 end
