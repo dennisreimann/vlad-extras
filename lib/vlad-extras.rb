79d3b851333d5765d85eb076ed69fbbf4f4333aa
Dennis Reimann <mail@dennisreimann.de>
2013-03-15 20:57:26 +0100

    Set defaults. Closes #17

--- lib/vlad-extras.rb
+++ lib/vlad-extras.rb
@@ -2,6 +2,9 @@
 require 'vlad-extras/database'
 require 'vlad-extras/remote'
 
+# set defaults
+set :bundle_cmd, "bundle"
+
 # load default recipes
 # symlinks is now obsolete, use vlad:update_symlinks instead
 %w(assets copy db deploy setup symlinks).each do |recipe|
